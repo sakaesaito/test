fe933b86c497b92af820d1646b76e8048f6492e4
sakae .s <user@s3110MacBook-Air.local>
2024-12-14 23:58:57 +0900

    “（12/14）”

--- main.js
+++ main.js
@@ -8,11 +8,16 @@ $(function(){
     //「.nav」に「.active」を追加・削除
     $(".drawer_nav").toggleClass("active");
   });
-  //メニュー内のリンクをクリックしたら
-  $(".drawer_list").click(function(){
-    //「.menu」から「.active」を削除
-    $(".menu").toggleClass("active");
-    //「.nev」から「.active」を削除
-    $(".drawer_nav").toggleClass("active");
+
+  $('.menu').on('click', function() {
+    $('.drawer_menu').removeClass('open');
   });
+
+  // //メニュー内のリンクをクリックしたら
+  // $(".drawer_list").click(function(){
+  //   //「.menu」から「.active」を削除
+  //   $(".menu").toggleClass("active");
+  //   //「.nev」から「.active」を削除
+  //   $(".drawer_nav").toggleClass("active");
+  // });
 });
