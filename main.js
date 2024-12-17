e0f65c2c7d36062f722f1e81a3923190e08eb2f2
sakae .s <user@s3110MacBook-Air.local>
2024-12-18 02:21:31 +0900

    “（12/18swaiper）”

--- main.js
+++ main.js
@@ -8,16 +8,5 @@ $(function(){
     //「.nav」に「.active」を追加・削除
     $(".drawer_nav").toggleClass("active");
   });
-
-  $('.menu').on('click', function() {
-    $('.drawer_menu').removeClass('open');
   });
-
-  // //メニュー内のリンクをクリックしたら
-  // $(".drawer_list").click(function(){
-  //   //「.menu」から「.active」を削除
-  //   $(".menu").toggleClass("active");
-  //   //「.nev」から「.active」を削除
-  //   $(".drawer_nav").toggleClass("active");
-  // });
-});
+  
