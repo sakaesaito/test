90478b31ab5dfa2219d7fac6fbe2149be12e6f68
sakae .s <user@s3110MacBook-Air.local>
2024-12-20 01:23:20 +0900

    “（12/19無限スライダー実装中）”

--- main.js
+++ main.js
@@ -10,4 +10,16 @@ $(function(){
   });
   });
   
+ 
+    const swiper = new Swiper('.logo-container', {
+      slidesPerView: 5, // 一度に表示するスライドの数
+      spaceBetween: 10, // スライド間の余白（ピクセル単位）
+      speed:10000,
+      loop: true, // ループさせる
+      allowTouchMove: false, 
+      autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+      },
+    });
   
