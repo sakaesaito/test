c1aa3ffcf7ee008ee4ecbb90e6f37a4754a2bda2
sakae .s <user@s3110MacBook-Air.local>
2024-12-26 19:40:32 +0900

    “（12/26無限スライダー修正）”

--- main.js
+++ main.js
@@ -14,21 +14,49 @@ $(function(){
 
   // 会社ロゴ無限ループのjQuery
  
-    const swiper = new Swiper('.logo-container', {
-      slidesPerView: 5, // 一度に表示するスライドの数
-      spaceBetween: 20, // スライド間の余白（ピクセル単位）
-      speed:50000,
+    const LoopSwiper = new Swiper('.swiper', {
+      speed:5000,
       loop: true, // ループさせる
       allowTouchMove: false, 
       autoplay: {
-        delay: 0,
-        disableOnInteraction: false,
+      delay: 0,
       },
+      breakpoints:{
+        0:{
+            slidesPerView: 2, // 一度に表示するスライドの数
+            spaceBetween: 63, // 要素間の余白
+        },
+        600:{
+            slidesPerView: 3,
+            spaceBetween: 63,
+        },
+       
+        1245:{
+            slidesPerView: 5,
+            spaceBetween: 80,
+        },
+    }
     });
 
 
 
-    // Q&A問い合わせ部分のjQuery
+    // // カードスライダーのjQuery
 
+    const swiper = new Swiper('.slider-sec-swiper', {
+      slidesPerView: 1, // 一度に表示するスライドの数
+      spaceBetween: 20, // スライド間の余白（ピクセル単位）
+      speed:1000,
+      loop: true, // ループさせる
+      allowTouchMove: false, 
+      // autoplay: {
+      //   delay: 0,
+      //   disableOnInteraction: false,
+      });
    
 
+    // // Navigation arrows
+    //   navigation, {
+    //   nextEL: '.swiper-button-next',
+    //   prevEL: '.swiper-button-prev',
+    //   },
+    //  );
