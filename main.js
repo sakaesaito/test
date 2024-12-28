fd2b39c32c813fe21e1dd73525349b009f81d2fa
sakae .s <user@s3110MacBook-Air.local>
2024-12-29 03:59:33 +0900

    “（12/28２つ目のスライダー修正途中）”

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(function(){
 
 
   // 会社ロゴ無限ループのjQuery
- 
+
     const LoopSwiper = new Swiper('.swiper01', {
       speed:4000,
       loop: true, // ループさせる
@@ -42,22 +42,32 @@ $(function(){
 
 
     // // // カードスライダーのjQuery
-
-    const swiper = new Swiper('.slider-sec', {
-      slidesPerView: 1, // 一度に表示するスライドの数
-      spaceBetween: 20, // スライド間の余白（ピクセル単位）
+ 
+    const swiper = new Swiper('.slider-sec .swiper' , {
       speed:1000,
       loop: true, // ループさせる
-      allowTouchMove: false, 
-      // autoplay: {
-      //   delay: 0,
-      //   disableOnInteraction: false,
-      });
-   
+      allowTouchMove: true, 
+      grabCursor:true, // 手でスワイプする
+      breakpoints:{
+          0:{
+              spaceBetween: 30, // 要素間の余白
+              slidesPerView: 1, // 一度に表示するスライドの数
+              slidesPerGroup: 1, // 一度にスワイプされるスライドの数
+          },
+        
+          900:{
+              spaceBetween: 70, // 要素間の余白
+              centeredSlides:true, // 中心を起点にする
+              slidesPerView: 2, // 一度に表示するスライドの数
+              slidesPerGroup: 2, // 一度にスワイプされるスライドの
+          },
+        
+  
 
-    // // Navigation arrows
-      navigation, {
+     // Navigation arrows
+      navigation: {
       nextEL: '.swiper-button-next',
       prevEL: '.swiper-button-prev',
       }
-    // });
+    }
+  });
